879cf100e4df58c16830975f48038723837c30ad
Roman <roman.markanych@gmail.com>
2023-04-10 01:15:48 +0300

    created preloader and simple auth

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+(() => {
+const preloader = document.getElementById("preloader");
+preloader.style.display = "block";
+setTimeout(() => preloader.style.display = "none",1000); 
+})()
+
+
+function createUser() {
+			const email = document.getElementById("email").value;
+			const name = document.getElementById("name").value;
+
+			if (email && name) {
+				document.getElementById("form").style.display = "none";
+				document.getElementById("userName").innerText = name;
+				document.getElementById("greeting").style.display = "block";
+			} else {
+				alert("Please, fill all fields");
+			}
+		}
