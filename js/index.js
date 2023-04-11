592327ff798fd94275d80d538843adb87bf3363a
Roman <roman.markanych@gmail.com>
2023-04-12 02:46:05 +0300

    added ability to earn points and to restart the game

--- js/index.js
+++ js/index.js
@@ -1,20 +1,162 @@
-
-(() => {
+// Preloader 
+function hidePreloader() {
+    // Selecting DOM element
     const preloader = document.getElementById("preloader");
+
+    //Update UI
     preloader.style.display = "block";
-    setTimeout(() => preloader.style.display = "none", 1000);
-})()
 
+    // use setTimeout to hide preloader element after 1 second
+    setTimeout(() => {
+        preloader.style.display = "none";
+    }, 1000);
+}
 
-function createUser() {
-    const email = document.getElementById("email").value;
-    const name = document.getElementById("name").value;
+hidePreloader();
 
-    if (email && name) {
-        document.getElementById("form").style.display = "none";
-        document.getElementById("userName").innerText = name;
-        document.getElementById("greeting").style.display = "block";
+// Selecting DOM elements
+const form = document.getElementById('form');
+const greeting = document.getElementById('greeting');
+const logOutBtn = document.getElementById('logOut');
+const logOutWrapper = document.getElementById('logOutWrapper');
+const userName = document.getElementById('userName');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const scoreElement = document.getElementById("score")
+const lvlElement = document.getElementById("lvl")
+const clickBtn = document.getElementById("earnPoint")
+const restart = document.getElementById("restart")
+const enemyWrapper = document.getElementById("enemy")
+let score = 0;
+let lvl = 1;
+let clicks = 0;
+
+const enemyPic = [
+    '1.png',
+    '2.png',
+    '3.png',
+    '4.png',
+    '5.png'
+]
+const maxClicksPerLvl = [5, 10, 15, 20, 25]
+
+//Arrow function to capitalize first letter
+const capitalizeName = n => n[0].toUpperCase() + n.slice(1);
+
+// Function to handle form 
+function createUser(e) {
+    e.preventDefault();
+
+    // Get input values and trim whitespace
+    const name = nameInput.value.trim();
+    const email = emailInput.value.trim();
+
+    // Store user data in local storage
+    localStorage.setItem('user', JSON.stringify({ name, email }));
+    hidePreloader();
+    // Update UI to show user is logged in
+    form.style.display = 'none';
+    userName.innerText = capitalizeName(name);
+    greeting.style.display = 'block';
+    logOutBtn.style.display = 'block';
+    logOutWrapper.style.display = 'flex';
+    document.body.style.backgroundImage = 'url("../images/bg-theme.jpg")'
+}
+
+// Function to logout
+function logoutUser() {
+    resetGame()
+    // Remove user data from local storage
+    localStorage.removeItem('user');
+
+    hidePreloader();
+    // Update UI to show user is logged out
+    form.style.display = 'block';
+    greeting.style.display = 'none';
+    logOutBtn.style.display = 'none';
+    logOutWrapper.style.display = 'none';
+    document.body.style.backgroundImage = 'url("../images/bg.png")'
+    // Clear input values
+    nameInput.value = '';
+    emailInput.value = '';
+}
+
+// Check if user is already logged in and update UI
+function checkLoggedInUser() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+        const { name } = user;
+        form.style.display = 'none';
+        userName.innerText = capitalizeName(name);
+        greeting.style.display = 'block';
+        logOutBtn.style.display = 'block';
+        logOutWrapper.style.display = 'flex';
+        document.body.style.backgroundImage = 'url("../images/bg-theme.jpg")'
     } else {
-        alert("Please, fill all fields");
+        logOutWrapper.style.display = 'none';
     }
 }
+
+// Event listener for form 
+form.addEventListener('submit', createUser);
+
+// Event listener for log out button
+logOutBtn.addEventListener('click', logoutUser);
+
+
+// Call function to check for logged in user on page load
+checkLoggedInUser();
+
+function updateScore() {
+    scoreElement.textContent = `Score: ${score}`
+}
+
+function updateLvl() {
+    lvlElement.textContent = `Level: ${lvl}`
+}
+
+function updateEnemy() {
+   
+    const enemyIndex = Math.floor(clicks / 5);
+   
+    if (enemyIndex < enemyPic.length) {
+        const enemyUrl = `./images/${enemyPic[enemyIndex]}`;
+        enemyWrapper.style.backgroundImage = `url('${enemyUrl}')`;
+        enemyWrapper.style.backgroundSize = 'contain';
+        enemyWrapper.style.backgroundRepeat =  "no-repeat";
+    }
+}
+
+function updateClicks() {
+    clicks++
+    if (clicks % maxClicksPerLvl[lvl - 1] === 0) {
+        lvl++
+        if (lvl <= maxClicksPerLvl.length) {
+            updateLvl();
+            updateEnemy(); 
+            alert(`Congratulations! You have reached level ${lvl} with ${clicks} clicks!`);
+            hidePreloader()
+        } else {
+            alert(`Congratulations! You have won the game with ${clicks} clicks!`);
+            clickBtn.disabled = true;
+        }
+       
+    }
+    score++;
+    updateScore();
+}
+
+clickBtn.addEventListener('click', updateClicks)
+
+function resetGame() {
+    hidePreloader();
+    score = 0;
+    lvl = 1;
+    clicks = 0;
+    updateScore();
+    updateLvl();
+    clickBtn.disabled = false;
+    enemyWrapper.style.backgroundImage = `url('/images/1.png')`
+}
+
+restart.addEventListener('click',resetGame)
